9febd2236502bc4ec68851a87a60edcac3cd75d3
ShineLynn <sla2482005@gmail.com>
2025-03-10 12:51:33 +0630

    Logo Name change

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -30,7 +30,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('Filament Demo')
+            ->brandName('Project Management')
             ->colors([
                 'primary' => Color::Amber,
             ])
